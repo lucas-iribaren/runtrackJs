ab49585c21a3329562960ec8a87f2fc379d65a6a
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-03-25 16:07:43 +0100

    [ajout]([job04]): programme vérification année bisextile

---
+++ jour01/job04/script.js
@@ -0,0 +1,22 @@
+function bisextile(annee) {
+  if (annee % 4 === 0){
+    if (annee % 100 === 0 && annee % 400 !== 0){
+      return false;
+    } else {
+      return true;
+    }
+} else {
+    return false;
+}
+}
+
+function verifierBissextile() {
+  var  anneeInput = document.getElementById("anneeInput").value;
+  var resultat = document.getElementById("resultat")
+
+  if (bisextile(anneeInput)){
+    resultat.textContent = "L'année " + anneeInput + " est bisextile"
+  } else {
+    resultat.textContent = "L'année "+ anneeInput + " n'est pas bisextile"
+  }
+}
