8d9f505de0f4ba1978a5740f5309ac276ce46421
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-03-26 13:02:41 +0100

    [ajout]([job04]): capture de touches pour manipulation de contenu

---
+++ jour02/job04/index.html
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 02</title>
+</head>
+<body>
+<main>
+    <textarea id="keylogger" rows="5" cols="50" placeholder="Tapez quelque chose ici..."></textarea>
+    <script src="script.js"></script>
+</main>
+</body>
+</html>
---
+++ jour02/job04/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('keydown', function(event) {
+  // Vérifie si la touche pressée est une lettre de a à z
+  if (event.key.match(/^[a-z]$/i)) {
+      var textarea = document.getElementById('keylogger');
+      // Vérifie si le focus est sur le textarea
+      if (document.activeElement === textarea) {
+          // Ajoute la lettre deux fois si le focus est sur le textarea
+          textarea.value += event.key ;
+      } else {
+          // Ajoute la lettre une fois si le focus n'est pas sur le textarea
+          textarea.value += event.key;
+      }
+  }
+});
