445f66b6a5046ef5d93f5b3dafde156e65afba90
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-03-28 10:42:17 +0100

    [ajout]([job02]): Pour mélanger et reformer un puzzle

---
+++ jour03/job02/index.html
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 02</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Puzzle Arc-En-Ciel</h1>
+        <div id="arc-en-ciel">
+            <img src="arc1.png" class="rainbow-img" alt="arc-en-ciel" >
+            <img src="arc2.png" class="rainbow-img" alt="arc-en-ciel" >
+            <img src="arc3.png" class="rainbow-img" alt="arc-en-ciel" >
+            <img src="arc4.png" class="rainbow-img" alt="arc-en-ciel" >
+            <img src="arc5.png" class="rainbow-img" alt="arc-en-ciel" >
+            <img src="arc6.png" class="rainbow-img" alt="arc-en-ciel" >
+        </div>
+        <button id="melanger" onclick="melanger()">Mélanger</button>
+
+        <p id="message"></p>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+        <script src="script.js"></script>
+    </main>
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    // Fonction pour mélanger les images
+    function melanger() {  
+        var images = $('.rainbow-img');
+        for (var i = 0; i < images.length; i++) {
+            var randomIndex = Math.floor(Math.random() * images.length);
+            var temp = images[i].src;
+            images[i].src = images[randomIndex].src;
+            images[randomIndex].src = temp;
+        }
+    }
+
+    // Fonction pour vérifier si les images sont dans le bon ordre
+    function checkOrder() {
+        var ordered = true;
+        var images = $('.rainbow-img');
+        var arcs = ['arc1', 'arc2', 'arc3', 'arc4', 'arc5', 'arc6'];
+        images.each(function (index) {
+            if (!$(this).attr('src').includes(arcs[index])) {
+                ordered = false;
+                return false; // Sortir de la boucle each dès qu'une image est mal placée
+            }
+        });
+        return ordered;
+    }
+
+    // Bouton pour mélanger les images
+    $('#melanger').click(function() {
+        melanger();
+        $('#message').text('');
+    });
+
+    // Contrôler l'ordre des images après un drag and drop
+    $('#arc-en-ciel').sortable({
+        revert: true,
+        stop: function() {
+            if(checkOrder()) {
+                $('#message').text('Vous avez gagné').css('color', 'green');
+            } else {
+                $('#message').text('Vous avez perdu').css('color', 'red');
+            }
+        }
+    });
+});
